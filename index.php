14b2717d9a49c5bd39e58a839c40a3e3a21092b1
Jacopo Calabrese <calabrese.jacopo98@gmail.com>
2025-11-08 17:43:47 +0100

    terminato selfwork 1

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+$integer = 10;
+$float = 10.1;
+$string = "ciao";
+$boolean = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+const INTERO = 10;
+const DECIMALE = 10.1;
+const STRINGA = "ciao";
+const BOOLEANO = true;
+
+var_dump (INTERO);
+var_dump (DECIMALE);
+var_dump (STRINGA);
+var_dump (BOOLEANO);
